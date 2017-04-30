d1eab1eef035551da53b6a6e0bda2a92bd6ab1e8
patryk <perseusz91@gmail.com>
2017-04-30 15:43:48 +0200

    zadanie9.1

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+var triangle1Area = getTriangleArea(10, 15);
+
+var triangle2Area = getTriangleArea(48, 12);
+
+var triangle3Area = getTriangleArea(2, 8);
+
+
+
+function getTriangleArea(a, h) {
+	
+	if ( (a > 0) && (h > 0) ) {
+	
+	return a*h/2 {
+	
+	} else if ( (a < 0) && (h < 0) ) {
+	
+	alert('Nieprawid³owe dane');
+	
+	} else ( (a == 0) && (h == 0) ) {
+	
+	alert('Nieprawid³owe dane');
+
+	}
+
+}
