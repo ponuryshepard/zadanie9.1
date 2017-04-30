f3a778766388ca28cf0c186a70d5b3b2031503da
patryk <perseusz91@gmail.com>
2017-04-30 19:32:17 +0200

    poprawiony js

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,13 @@ var triangle2Area = getTriangleArea(48, 12);
 var triangle3Area = getTriangleArea(2, 8);
 
 
+var wynik = a*h/2;
 
 function getTriangleArea(a, h) {
 	
 	if ( (a > 0) && (h > 0) ) {
 	
-	return a*h/2 {
-	
+	return wynik;
 	} else if ( (a < 0) && (h < 0) ) {
 	
 	alert('Nieprawid³owe dane');
