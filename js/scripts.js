d5325d1c7de51e17309634dc34748b0a251d316d
patryk <perseusz91@gmail.com>
2017-05-03 17:16:20 +0200

    nowyjs

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,23 @@
-var triangle1Area = getTriangleArea(10, 15);
-
-var triangle2Area = getTriangleArea(48, 12);
-
-var triangle3Area = getTriangleArea(2, 8);
-
-
-var wynik = a*h/2;
-
 function getTriangleArea(a, h) {
 	
-	if ( (a > 0) && (h > 0) ) {
-	
-	return wynik;
-	} else if ( (a < 0) && (h < 0) ) {
-	
-	alert('Nieprawid³owe dane');
-	
-	} else ( (a == 0) && (h == 0) ) {
-	
-	alert('Nieprawid³owe dane');
+	if ( (a  0) && (h  0) ) {
+		
+		var wynik = ah2;
+		
+		return wynik;
 
+	} else {
+	
+		alert('Nieprawid³owe dane');
+	
 	}
 
 }
+
+
+
+var triangle1Area = getTriangleArea(10, 15);
+
+var triangle2Area = getTriangleArea(0, 12);
+
+var triangle3Area = getTriangleArea(2, 8);
